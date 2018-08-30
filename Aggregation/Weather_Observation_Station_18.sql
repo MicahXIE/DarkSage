14917ccefab4ff900e6c34be520b3c615dd216d7
Micah007PKG <bin.xie.sg@gmail.com>
2018-08-31 00:43:47 +0800

    update

---
+++ Aggregation/Weather_Observation_Station_18.sql
@@ -0,0 +1,8 @@
+／*
+SQL - Aggregation - Weather Observation Station 18
+
+例如在平面上，坐标（x1, y1）的i点与坐标（x2, y2）的j点的曼哈顿距离为：
+d(i,j)=|X1-X2|+|Y1-Y2|.
+*／
+
+SELECT ROUND(ABS(MAX(LAT_N) - MIN(LAT_N)) + ABS(MAX(LONG_W) - MIN(LONG_W)),4) FROM STATION;
