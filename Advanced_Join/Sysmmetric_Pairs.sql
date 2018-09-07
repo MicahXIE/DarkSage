e95b2ee361c0ff80e7325d637449282247dcfe62
Micah007PKG <bin.xie.sg@gmail.com>
2018-09-08 00:13:01 +0800

    update

--- Advanced_Join/Sysmmetric_Pairs.sql
+++ Advanced_Join/Sysmmetric_Pairs.sql
@@ -4,6 +4,42 @@ SQL - Advanced Join - Sysmmetric Pairs
 
 */
 
+/*
+
+mysql> select * from Functions;
++----+----+
+| X  | Y  |
++----+----+
+| 20 | 20 |
+| 20 | 20 |
+| 20 | 21 |
+| 23 | 22 |
+| 22 | 23 |
+| 21 | 20 |
++----+----+
+
+INNER JOIN A, B (every line of B to match A)
+mysql> SELECT A.X,A.Y,B.X,B.Y FROM Functions A INNER JOIN Functions B ON B.Y = A.X;
++----+----+----+----+
+| X  | Y  | X  | Y  |
++----+----+----+----+
+| 20 | 20 | 20 | 20 |
+| 20 | 20 | 20 | 20 |
+| 20 | 21 | 20 | 20 |
+| 20 | 20 | 20 | 20 |
+| 20 | 20 | 20 | 20 |
+| 20 | 21 | 20 | 20 |
+| 21 | 20 | 20 | 21 |
+| 22 | 23 | 23 | 22 |
+| 23 | 22 | 22 | 23 |
+| 20 | 20 | 21 | 20 |
+| 20 | 20 | 21 | 20 |
+| 20 | 21 | 21 | 20 |
++----+----+----+----+
+
+
+*/
+
 SELECT A.X, A.Y FROM Functions A 
 			    INNER JOIN Functions B ON B.Y = A.X 
 			    WHERE A.Y >= A.X AND B.X = A.Y 
