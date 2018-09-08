617a6aaaa5a5c85f39f9238b8c3b49a45d540163
Micah007PKG <bin.xie.sg@gmail.com>
2018-09-09 00:32:52 +0800

    update

--- Alternative_Queries/Draw_The_Triangle1.sql
+++ Alternative_Queries/Draw_The_Triangle1.sql
@@ -2,5 +2,11 @@
 SQL - Alternative Queries - Draw The Triangle 1
 */
 
+# MYSQL
 SET @NUMBER:=21;
 SELECT REPEAT('* ', @NUMBER:=@NUMBER-1) FROM information_schema.tables;
+
+# ORACLE
+
+select rpad('*', x,' *') from 
+( SELECT LEVEL x FROM DUAL CONNECT BY LEVEL <= 40 Order by Level desc ) where mod(x,2) = 0;
