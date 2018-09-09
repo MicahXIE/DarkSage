c6e4b66ce00a05ec9a7e8171a6bbaca2df4e956b
Micah007PKG <bin.xie.sg@gmail.com>
2018-09-10 01:36:09 +0800

    update

--- Alternative_Queries/Print_Prime_Numbers.sql
+++ Alternative_Queries/Print_Prime_Numbers.sql
@@ -27,3 +27,28 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(SUBSTR(output,0,length(output)-1));
 END;
 /
+
+
+
+# MYSQL num, nu:1,1000 NOT EXISTS
+# The FLOOR() function returns the largest integer value 
+#that is less than or equal to a number.
+
+SELECT GROUP_CONCAT(NUMB SEPARATOR '&')
+FROM (
+    SELECT @num:=@num+1 as NUMB FROM
+    information_schema.tables t1,
+    information_schema.tables t2,
+    (SELECT @num:=1) tmp
+) tempNum
+WHERE NUMB<=1000 AND NOT EXISTS(
+        SELECT * FROM (
+            SELECT @nu:=@nu+1 as NUMA FROM
+                information_schema.tables t1,
+                information_schema.tables t2,
+                (SELECT @nu:=1) tmp1
+                LIMIT 1000
+            ) tatata
+        WHERE FLOOR(NUMB/NUMA)=(NUMB/NUMA) AND NUMA<NUMB AND NUMA>1
+)
+
