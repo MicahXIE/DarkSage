9bb505a5258636172bdbac963fd2e3b5efed1527
MicahXIE <bin.xie.sg@gmail.com>
2018-11-21 22:54:25 +0800

    update

--- Advanced_Join/15_Days_Of_Learning_SQL.sql
+++ Advanced_Join/15_Days_Of_Learning_SQL.sql
@@ -20,7 +20,7 @@ submission_date ,
 
 ( SELECT COUNT(distinct hacker_id)  
  FROM Submissions s2  
- WHERE s2.submission_date = s1.submission_date AND    (SELECT COUNT(distinct s3.submission_date) FROM 
+ WHERE s2.submission_date = s1.submission_date AND (SELECT COUNT(distinct s3.submission_date) FROM 
  Submissions s3 WHERE s3.hacker_id = s2.hacker_id AND s3.submission_date < s1.submission_date) 
  = dateDIFF(s1.submission_date , '2016-03-01')) ,
 
